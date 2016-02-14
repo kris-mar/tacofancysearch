1e40b8d785d6fc1c6542083dc560aa90e1fa2e4d
Kristen Hanlon <kmh0394@gmail.com>
2016-02-14 17:47:50 -0500

    Adding jQuery animations, fixing a nav bar

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,28 @@
-$(".dropdown-toggle").toggle(
-function() {
-    $(".dropdown-content").slideDown("fast").show();
-}, 
-function() {
-    $(".dropdown-content").hide();
+$(function() {
+  $('.dropdown-title').click(function () {
+    $(".dropdown-content").toggle("slow");
+  });
+    $(".feedme").click(function () {
+        $(".dropdown-content").hide("slow");
+    });
+    $(".taco").click(function() {
+        $('html,body').animate({
+                scrollTop: $(".red-container").offset().top},
+            'slow');
+    });
+
+    function scrollFunction() {
+        var sPos = document.body.scrollTop;
+
+        if(sPos > (window.innerHeight - ($(".nav").height()/2))) {
+            $(".nav").show("slow")
+            $(".nav").css('top', $(".nav").height()/2);
+        }
+        else {
+            $(".nav").hide("slow")
+            $(".nav").css('display', 'none');
+        }
+    }
+
+    window.onscroll = scrollFunction;
 });
